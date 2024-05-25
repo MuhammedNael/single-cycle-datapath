4c7f8edf0f82d1badb0b04cef2d5def328a4172a
Muhammed Nael <imohmad7@gmail.com>
2024-05-25 18:53:28 +0300

    finished

--- singlecycleMIPS-lite-commented/alu32.v
+++ singlecycleMIPS-lite-commented/alu32.v
@@ -1,11 +1,17 @@
-module alu32(sum,a,b,zout,gin);//ALU operation according to the ALU control line values
+module alu32(sum, a, b, zout, gin, Nin, Vin);//ALU operation according to the ALU control line values
 output [31:0] sum;
 input [31:0] a,b; 
 input [2:0] gin;//ALU control line
+
 reg [31:0] sum;
 reg [31:0] less;
+
 output zout;
+output Nin, Vin;
+
 reg zout;
+reg Nin, Vin;
+
 always @(a or b or gin)
 begin
 	case(gin)
@@ -17,8 +23,24 @@ begin
 		  end
 	3'b000: sum=a & b;	//ALU control line=000, AND
 	3'b001: sum=a|b;		//ALU control line=001, OR
+	
+	3'b100: sum = ~(a | b); // ALU control line = 100, NOR
+        3'b011: sum = ~(a & b); // ALU control line = 011, NAND
 	default: sum=31'bx;	
 	endcase
-zout=~(|sum);
+
+	zout=~(|sum); // Zin
+	Nin = sum[31]; // negative flag: 1 if result is negative
+
+	// overflow detect
+	case (gin)
+      	3'b010: // ADD overflow detection
+        	Vin = (a[31] & b[31] & ~sum[31]) | (~a[31] & ~b[31] & sum[31]);
+      	3'b110: // SUB overflow detection
+        	Vin = (a[31] & ~b[31] & ~sum[31]) | (~a[31] & b[31] & sum[31]);
+      	default:
+        	Vin = 0;
+    	endcase
+
 end
 endmodule
